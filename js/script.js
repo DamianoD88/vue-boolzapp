ba62373fe06c3e57b3db0f248b52cefe88ab2711
DamianoD88 <dispe88@live.it>
2021-06-29 15:56:12 +0200

    add 11

--- js/script.js
+++ js/script.js
@@ -4,19 +4,98 @@ const app = new Vue({
       count:0,
       contacts: [
         {
-            name: 'Mchele',
-            avatar: '',
+            name: 'Michele',
+            avatar: '_1',
             visible: true,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
                     status: 'sent'
-                }
+                },
+                {
+                    date: '10/01/2020 15:50:55',
+                    text: 'Ha magnato??',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:50:55',
+                    text: 'sta bello satollo!',
+                    status: 'received'
+                },
             ]
-
-
-        }
+ 
+        },
+        {
+            name: 'Domenico',
+            avatar: '_2',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:55',
+                    text: 'Ha magnato??',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:50:55',
+                    text: 'sta bello satollo!',
+                    status: 'received'
+                },
+            ]
+ 
+        },
+        {
+            name: 'Gofredo',
+            avatar: '_3',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:55',
+                    text: 'Ha magnato??',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:50:55',
+                    text: 'sta bello satollo!',
+                    status: 'received'
+                },
+            ]
+ 
+        },
+        {
+            name: 'Giulia',
+            avatar: '_io',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:55',
+                    text: 'Ha magnato??',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:50:55',
+                    text: 'sta bello satollo!',
+                    status: 'received'
+                },
+            ]
+ 
+        },
       ]
     }
+   
   })
